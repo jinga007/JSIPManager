3ca27ffb151699cd31dea54fd1dff0787b24f14d
HyunOJin <jinga007@naver.com>
2022-07-16 01:46:42 +0900

    TEST Commit

--- JSIPManager/Windows/PortScanWindow.xaml.cs
+++ JSIPManager/Windows/PortScanWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +20,80 @@ namespace JSIPManager.Windows
     /// </summary>
     public partial class PortScanWindow : Window
     {
+        System.Threading.Thread td = null;
+        bool bStop = false;
         public PortScanWindow()
         {
             InitializeComponent();
+        }
+
+
+        public void DoPortScan(object data)
+        {
+            string[] IPInfors = (data as string).Split('=');
+            if (IPInfors.Length > 0 && (data as string).Contains("="))
+            {
+                this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Background, new Action(() =>
+                {
+                    txtPortScanStatus.Document.Blocks.Add(new Paragraph(new Run($"Port scan start..... ")));
+
+                }));
+
+                string[] IPAddresses = IPInfors[0].Split(',');
+                string[] Ports = IPInfors[1].Split(',');
+                TcpClient Scan = new TcpClient();
+                foreach (var ip in IPAddresses)
+                {
+                    foreach(var port in Ports)
+                    {
+                        if (bStop)
+                            break;
+
+                        int iPort = -1;
+                        int.TryParse(port, out iPort);
+
+                        try
+                        {
+                            Scan.Connect(ip, iPort);
+                            this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Background, new Action(() =>
+                            {
+                                txtPortScanStatus.Document.Blocks.Add(new Paragraph(new Run($"IP ADDRESS [{ip.ToString()}], Port [{iPort.ToString()}] is opened")));
+
+                            }));
+                        }
+                        catch
+                        {
+                            this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Background, new Action(() =>
+                            {
+                                txtPortScanStatus.Document.Blocks.Add(new Paragraph(new Run($"IP ADDRESS [{ip.ToString()}], Port [{iPort.ToString()}] is not opened")));
+
+                            }));
+
+                        }
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                }
+                this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Background, new Action(() =>
+                {
+                    txtPortScanStatus.Document.Blocks.Add(new Paragraph(new Run($"Port scan end..... ")));
+
+                }));
+            }
+        }
+
+        private void btnPortScanStart_Click(object sender, RoutedEventArgs e)
+        {
+
+            string strIpInfors = $"{txtIPAddress.Text}={txtPort.Text}";
+            bStop = false;
+            txtPortScanStatus.Document.Blocks.Clear();
+            td = new System.Threading.Thread(DoPortScan);
+            td.Start(strIpInfors);
+        }
+
+        private void btnPortScanStop_Click(object sender, RoutedEventArgs e)
+        {
+            bStop = true;
         }
     }
 }
